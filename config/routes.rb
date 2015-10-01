41ee9745f4316d91cbaecdf27758f766a1ae2041
RinSer <serj.dukareff@gmail.com>
2015-10-01 11:29:44 +0300

    Third

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'recipes_controller/index'
 
+  # Set Default Page
+  root 'recipes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
